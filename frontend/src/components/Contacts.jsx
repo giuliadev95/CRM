55a47a2ee1e8bc48e3c78219136241b5af7a4521
Giulia Moukouyou <giuliamoukouyou@gmail.com>
2025-09-21 09:44:16 +0200

    Add search function logic in the frontend and its query in the backend, along with the route.

--- frontend/src/components/Contacts.jsx
+++ frontend/src/components/Contacts.jsx
@@ -1,7 +1,37 @@
 // Contacts.jsx
 import React from "react";
+import axios from "axios";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const Contacts = ({ contacts, loading }) => {
+const Contacts = ({ contacts, loading }) => { 
+
+  // constants I need, moved here  rom the ContactsList.jsx component
+  const [input, setInput] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredContacts, setFilteredContacts]= useState([]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const cleanedInput = input.trim().toLowerCase().replace(/\s+/g, '');
+    setSearchTerm(cleanedInput);
+
+    try {
+      const res = await axios.get("http://localhost:3000/api/contacts/search/contact", {
+        params: { q: cleanedInput }, 
+      });
+
+      setFilteredContacts(res.data);
+    } catch (err) {
+      console.error("Error in the query:", err);
+    }
+  }
+
+   useEffect(() => {
+    setFilteredContacts(contacts);
+  }, [contacts]);
+
   if (loading) {
     return <h2>Loading...</h2>;
   }
@@ -11,19 +41,63 @@ const Contacts = ({ contacts, loading }) => {
   }
 
   return (
-    <ul>
-      {contacts.map((contact) => (
-        <li key={contact.id_contact}>
-          <table className="table border table-hover">
-            <tbody>
-              <tr>
-                <td>{contact.name}</td>
+    <div>  
+      <table className="table border table-hover">
+          <thead>
+            <tr>
+              <td colspan="100">
+                  <form class="d-flex w-100"onSubmit={handleSubmit} >
+                      <input 
+                          class="form-control me-2 w-100" 
+                          type="search" 
+                          placeholder="Cerca" 
+                          aria-label="Search"  
+                          value={input}
+                          onChange={(e)=> setInput(e.target.value)}         
+                      />
+                      <button class="btn btn-light border border-secondary " type="submit">Cerca</button>                 
+                  </form>  
+              </td>
+            </tr>
+            <tr>
+              <th scope="col">Nome</th>
+              <th scope="col">Cognome</th>
+              <th className="d-none d-md-table-cell" scope="col">Telefono</th>
+              <th className="d-none d-md-table-cell" scope="col">Email</th>
+              <th className="d-none d-md-table-cell" scope="col">Ruolo</th>
+              <th className="d-none d-md-table-cell" scope="col">Azienda</th>
+              <th></th>
+            </tr>
+          </thead>
+          {/* Map the fetched contacts to display each of them in a table row */}
+          <tbody>
+            {filteredContacts.length > 0 ? (
+              filteredContacts.map((contact) => (
+              <tr key={contact.id_contact}>
+                <td className="hover:underline cursor-pointer">{contact.name}</td>
+                <td className="hover:underline cursor-pointer">{contact.surname}</td>
+                <td className="d-none d-md-table-cell hover:underline cursor-pointer">{contact.phone}</td>
+                <td className="d-none d-md-table-cell hover:underline cursor-pointer">{contact.email}</td>
+                <td className="d-none d-md-table-cell hover:underline cursor-pointer">{contact.role}</td>
+                <td className="d-none d-md-table-cell hover:underline cursor-pointer">{contact.company_name || "-"}</td>
+                <td>
+                  <div  
+                      className="bg-transparent border-none shadow-none p-0 m-0 outline-none">
+                      <BsThreeDotsVertical/>
+                  </div>
+                </td>
               </tr>
-            </tbody>
-          </table>
-        </li>
-      ))}
-    </ul>
+              ))
+              ) : (
+              <tr>
+                <td colSpan="7" className="text-center text-muted">
+                  Nessun contatto presente
+                </td>
+              </tr>
+            )}
+        </tbody>
+    </table>
+  </div>
   );
 };
 
--- frontend/src/pages/contacts/ContactsList.jsx
+++ frontend/src/pages/contacts/ContactsList.jsx
@@ -1,10 +1,6 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom"; // Import useNavigate to redirect to configured routes without refreshing the page thanks to SPA
-//import { AiTwotoneDelete } from "react-icons/ai"; // bin icon to delete a contact
-//import { FaPen } from "react-icons/fa"; // pen to update a contact
-//import { IoMdEye } from "react-icons/io";
-import { BsThreeDotsVertical } from "react-icons/bs";
-import axios from "axios";
 import Contacts from "@/components/Contacts";
 import Pagination from "@/components/Pagination";
 import '@styles/app.css'
@@ -16,39 +12,18 @@ const ContactsList = () => {
     // setError
     const [theError, setTheError] = useState(null);
 
-
-    // Store the search input value and handles the input change
-    const [input, setInput] = useState("");
-    const [searchTerm, setSearchTerm] = useState("");
-
     // Set loader
     const [loading, setLoading] = useState(false);
-    // Store the records number
-    //const [records, setRecords] = useState([]);
+
     // Set initial page
     const [page, setPage] = useState(1);
     const [contactsPerPage] = useState(10);
+
     // This constant hooks the "useNavigate()" functionalities to the "navigate" variable
     const navigate = useNavigate();
  
-    // useEffect() hook to fetch all contacts from the backend endpoint, when the DOM mounts
-    /*
-    useEffect(()=> {
-        fetch("http://localhost:3000/api/contacts/get")
-        .then ( (response) => response.json() )
-        .then( (data) => {
-
-            // The fetched data update the content of the contact variable. This happens even after the deletion of a single contact.
-            setContacts(data);
-
-            // OUTPUT EXPECTED: [ { array of objects fetched from localhost:3000/api } ] , object
-            console.log(data, typeof data);
-       })
-       .catch((err) => console.error(`There was an error fetching contacts: ${err}`));
-    },[]);
-    */
-
-    useEffect(()=> {
+    // useEffect to fetch the contacts from the PostgreSQL database, and to set the Loader
+       useEffect(()=> {
         const fetchContacts = async()=> {
             try{
                 setLoading(true);
@@ -58,7 +33,7 @@ const ContactsList = () => {
                 // The fetched data update the content of the contact variable. This happens even after the deletion of a single contact.
                 setContacts(response.data);
     
-                // OUTPUT EXPECTED: [ { array of objects fetched from localhost:3000/api } ] , object
+                // EXPECTED OUTPUT: [ An array of objects { }, { },... ] , "object".
                 console.log(response.data, typeof response.data);
                 setLoading(false);
 
@@ -77,57 +52,12 @@ const ContactsList = () => {
         };
         fetchContacts();
     },[]);
-    
-    
-    // Contacts filtered by the "Search" bar +
-    // Filter companies' names when you press the Search button
-        const filteredContacts = contacts.filter((contact) => {
-        const normalizedSearch = searchTerm.toLowerCase().replace(/\s+/g, '');
-        const normalizedName = contact?.name?.toLowerCase().replace(/\s+/g, '');
-        return normalizedName.includes(normalizedSearch);
-    });
-
-    // Function to handle the submit of the "searchbar" form
-    function handleSubmit(e) {
-        e.preventDefault();
-        // Sanitize input with regex: trims whitespaces and Lowercases everything
-        const cleanedInput = input.trim().toLowerCase().replace(/\s+/g, '');
-        setSearchTerm(cleanedInput);
-    }
-
-    // Delete contact
-    /*
-        function deleteContact(id) {
-            if (!id) return console.error("The ID is missing to perform the deletion.");
-
-            fetch(`http://localhost:3000/api/contact/delete/${id}`, {
-                method: "DELETE",
-            })
-            .then((res) => {
-                if (!res.ok) throw new Error("Error during the deletion.");
-                console.log(`Contact with ID: ${id} deleted successfully.`);
-                // Avoid mapping and filtering the deleted contact, as its ID will be missing.
-                setContacts(contacts.filter((contact) => contact.id_contact !== id));
-            })
-            .catch((err) => console.error(`Error: ${err}`));
-        }
-    */
 
     // Open the page to create a new contact
     function openForm() {
         navigate("/new-contact")
     }
 
-    /* 
-        Open the page to update the contact
-        const openContactPage = (id) => {
-            navigate(`/update-contact/${id}`);
-        }
-
-        const openContactView = (id) => {
-            navigate(`/contact-view/${id}`);
-        }
-    */
     // Get current contacts
     const indexOfLastContact = page * contactsPerPage;
     const indexOfFirstContact = indexOfLastContact - contactsPerPage;
@@ -137,12 +67,6 @@ const ContactsList = () => {
     // return:
     return (
         <>  
-            <Contacts
-                contacts={currentContacts} loading={loading}
-            />
-            <Pagination
-                contactsPerPage={contactsPerPage} totalContacts={contacts.length} paginate={paginate}
-            />
             <div className="mx-8">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
@@ -184,85 +108,13 @@ const ContactsList = () => {
                     </div>
                 </div>
             </div>
-            <div className="mx-4">
-                {/* Contact list table */}
-                <table class="table border table-hover">                
-                    <thead>
-                        <tr>
-                            <td colspan="100">
-                                <form class="d-flex w-100" onSubmit={handleSubmit}>
-                                    <input 
-                                        class="form-control me-2 w-100" 
-                                        type="search" 
-                                        placeholder="Search" 
-                                        aria-label="Search"
-                                        value={input}
-                                        onChange={(e)=> setInput(e.target.value)}
-                                    />
-                                    <button class="btn btn-light border border-secondary " type="submit">Cerca</button>                 
-                                </form>  
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="col"> Nome </th>
-                            <th scope="col">Cognome</th>
-                            <th className="d-none d-md-table-cell" scope="col"> Telefono</th>
-                            <th className="d-none d-md-table-cell"scope="col"> Email </th>
-                            <th className="d-none d-md-table-cell" scope="col"> Ruolo</th>
-                            <th className="d-none d-md-table-cell" scope="col"> Azienda</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {/* Map the fetched contacts to display each of them in a table row */}
-                        {filteredContacts.length > 0 ? (
-                            filteredContacts.map((contact) => (  
-                                <tr key={contact.id_contact}>
-                                    <td className="hover:underline cursor-pointer">{contact.name}</td>
-                                    <td className="hover:underline cursor-pointer">{contact.surname}</td>  
-                                    <td className="d-none d-md-table-cell hover:underline cursor-pointer">{contact.phone}</td>
-                                    <td className="d-none d-md-table-cell hover:underline cursor-pointer">{contact.email}</td>
-                                    <td className="d-none d-md-table-cell hover:underline cursor-pointer">{contact.role}</td>
-                                    <td className="d-none d-md-table-cell hover:underline cursor-pointer">{contact.company_name || "-"}</td>
-                                    {/**
-                                    <td className="d-none d-md-table-cell"> 
-                                    <button
-                                    type="button"
-                                    onClick={ ()=> openContactView(contact.id_contact) }
-                                    >
-                                    <IoMdEye/>
-                                    </button>   
-                                    
-                                    <button
-                                    type="button"
-                                    onClick={ ()=> deleteContact(contact.id_contact) }
-                                    >
-                                    <AiTwotoneDelete/>
-                                    </button>   
-                                    
-                                    
-                                    <button
-                                    type="button"
-                                    onClick={ ()=> openContactPage(contact.id_contact) }
-                                    >
-                                    <FaPen/>
-                                    </button>         
-                                    </td>
-                                    * 
-                                    */}
-                                    <td>
-                                        <div  
-                                            className="bg-transparent border-none shadow-none p-0 m-0 outline-none">
-                                            <BsThreeDotsVertical/>
-                                        </div>
-                                    </td>
-                                </tr>
-                            ))
-                        ) : (
-                            <p>Nessun contatto trovato</p>
-                        )}
-                    </tbody>
-                </table>
+            <div className="mx-8">
+                <Contacts
+                    contacts={currentContacts} loading={loading}
+                />
+                <Pagination
+                    contactsPerPage={contactsPerPage} totalContacts={contacts.length} paginate={paginate}
+                />
             </div>
         </>
     );
